ae021dd474e8e94f4c7123e6e6061909e0d47dcc
Changkyu Ahn <changkyu.ahn@navercorp.com>
2022-03-30 22:40:54 +0900

    1.2.2. 커넥션 만들기의 추출

--- src/main/java/io/github/positoy/UserDao.java
+++ src/main/java/io/github/positoy/UserDao.java
@@ -4,10 +4,13 @@ import java.sql.*;
 
 public class UserDao {
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://192.168.59.103:30000/toby", "root", "test1234");
+        return DriverManager.getConnection("jdbc:mysql://192.168.59.103:30000/toby", "root", "test1234");
+    }
 
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -19,9 +22,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://192.168.59.103:30000/toby", "root", "test1234");
-
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id=?");
         ps.setString(1, id);
 
