68f6b6c045f05cb47d99ff11041117074e9840ff
Changkyu Ahn <changkyu.ahn@navercorp.com>
2022-04-05 21:53:24 +0900

    2.3.3. UserDAO JUnit4 적용

--- src/test/java/io/github/positoy/UserDaoTest.java
+++ src/test/java/io/github/positoy/UserDaoTest.java
@@ -1,72 +1,67 @@
 package io.github.positoy;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.*;
+import org.junit.*;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
-class UserDaoTest {
+public class UserDaoTest {
     static UserDao userDao;
 
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeClass
+    public static void beforeAll() {
         log.info("before all - initialize userDao");
         ApplicationContext context = new GenericXmlApplicationContext("userDaoConfig.xml");
         userDao = context.getBean("userDao", UserDao.class);
     }
 
-    @AfterAll
-    static void afterAll() {
+    @AfterClass
+    public static void afterAll() {
         log.info("after all");
     }
 
-    @BeforeEach
-    void beforeEach() throws SQLException {
+    @Before
+    public void beforeEach() throws SQLException {
         log.info("before each - clear db");
         userDao.deleteAll();
 
     }
 
-    @AfterEach
-    void afterEach() throws SQLException {
+    @After
+    public void afterEach() throws SQLException {
         log.info("after each - clear db");
         userDao.deleteAll();
     }
 
     @Test
-    void add() throws SQLException {
+    public void add() throws SQLException {
         userDao.add(new User("test", "name", "password"));
     }
 
     @Test
-    void select() throws SQLException {
+    public void select() throws SQLException {
         userDao.add(new User("test1", "name", "password"));
         User user = userDao.get("test1");
-        assertEquals("test1", user.getId());
+        Assert.assertEquals("test1", user.getId());
     }
 
-    @Test
-    void emptyCase() throws SQLException {
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void emptyCase() throws SQLException {
         userDao.add(new User("test", "name", "pw"));
         userDao.add(new User("test1", "name", "pw"));
-        assertEquals(2, userDao.getCount());
+        Assert.assertEquals(2, userDao.getCount());
 
         userDao.add(new User("test2", "name", "pw"));
-        assertEquals(3, userDao.getCount());
+        Assert.assertEquals(3, userDao.getCount());
 
         userDao.deleteAll();
-        assertEquals(0, userDao.getCount());
+        Assert.assertEquals(0, userDao.getCount());
 
-        assertThrows(EmptyResultDataAccessException.class, () -> {
-            userDao.get("unknown_id");
-        });
+        userDao.get("unknown_id");
     }
 }
