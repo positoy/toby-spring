b79f1b01fc9d6703b9a60585729b4baf019fb703
Changkyu Ahn <changkyu.ahn@navercorp.com>
2022-04-06 15:18:05 +0900

    3.3.2. 전략과 클라이언트의 동거 - 익명내부클래스

--- src/main/java/io/github/positoy/UserDao.java
+++ src/main/java/io/github/positoy/UserDao.java
@@ -22,7 +22,7 @@ public class UserDao extends SqlOperation {
     }
 
     public void add(User user) throws SQLException {
-        class AddUserStatement implements StatementStrategy {
+        insert(new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
@@ -31,42 +31,38 @@ public class UserDao extends SqlOperation {
                 ps.setString(3, user.getPassword());
                 return ps;
             }
-        }
-        insert(new AddUserStatement());
+        });
     }
 
     public User get(String id) throws SQLException, EmptyResultDataAccessException {
-        class GetUserStatement implements StatementStrategy {
+        return select(new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("select * from users where id=?");
                 ps.setString(1, id);
                 return ps;
             }
-        }
-        return select(new GetUserStatement());
+        });
     }
 
     public void remove(String id) throws SQLException {
-        class DeleteUserStatement implements StatementStrategy {
+        delete(new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("delete from users where id=?");
                 ps.setString(1, id);
                 return ps;
             }
-        }
-        delete(new DeleteUserStatement());
+        });
     }
 
     public void removeAll() throws SQLException {
-        class DeleteAllStatement implements StatementStrategy {
+        delete(new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection c) throws SQLException {
                 return c.prepareStatement("delete from users");
             }
-        }
-        delete(new DeleteAllStatement());
+        });
     }
 
     public int getCount() throws SQLException {
