e5c0230cdfc87017eec0e4d1baa8f1e139739415
Changkyu Ahn <changkyu.ahn@navercorp.com>
2022-04-08 16:08:06 +0900

    functional interface 간략하게 수정

--- src/main/java/io/github/positoy/UserDao.java
+++ src/main/java/io/github/positoy/UserDao.java
@@ -2,7 +2,6 @@ package io.github.positoy;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -21,55 +20,40 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
-        jdbcContext.workWithStatementStrategyVoid(new StatementStrategy() {
-            @Override
-            public PreparedStatement makeStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
-                return ps;
-            }
+        jdbcContext.workWithStatementStrategyVoid(c -> {
+            PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+            return ps;
         });
     }
 
     public User get(String id) throws SQLException, EmptyResultDataAccessException {
-        return jdbcContext.workWithStatementStrategyUser(new StatementStrategy() {
-            @Override
-            public PreparedStatement makeStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("select * from users where id=?");
-                ps.setString(1, id);
-                return ps;
-            }
+        return jdbcContext.workWithStatementStrategyUser(c -> {
+            PreparedStatement ps = c.prepareStatement("select * from users where id=?");
+            ps.setString(1, id);
+            return ps;
         });
     }
 
     public void remove(String id) throws SQLException {
-        jdbcContext.workWithStatementStrategyVoid(new StatementStrategy() {
-            @Override
-            public PreparedStatement makeStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("delete from users where id=?");
-                ps.setString(1, id);
-                return ps;
-            }
+        jdbcContext.workWithStatementStrategyVoid(c -> {
+            PreparedStatement ps = c.prepareStatement("delete from users where id=?");
+            ps.setString(1, id);
+            return ps;
         });
     }
 
     public void removeAll() throws SQLException {
-        jdbcContext.workWithStatementStrategyVoid(new StatementStrategy() {
-            @Override
-            public PreparedStatement makeStatement(Connection c) throws SQLException {
-                return c.prepareStatement("delete from users");
-            }
+        jdbcContext.workWithStatementStrategyVoid(c -> {
+            return c.prepareStatement("delete from users");
         });
     }
 
     public int getCount() throws SQLException {
-        return jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makeStatement(Connection c) throws SQLException {
-                return c.prepareStatement("select count(1) from users");
-            }
+        return jdbcContext.workWithStatementStrategy(c -> {
+            return c.prepareStatement("select count(1) from users");
         });
     }
 }
